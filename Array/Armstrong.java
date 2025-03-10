32fc4000c9f1d8ec7ae67a8911b608b320e8f522
mdsohail12 <mdsohailalam612@gmail.com>
2025-03-10 11:54:06 +0530

    Armstrong

---
+++ Array/Armstrong.java
@@ -0,0 +1,24 @@
+public class Armstrong {
+        static void check_armstrong(int n){
+         int temp=n;
+         int sum=0;
+         while(n>0){
+             int rem=n%10;
+             sum+=rem*rem*rem;
+             n=n/10;
+         }
+     
+         if(temp==sum){
+             System.out.println("Armstrong");
+         }
+         else{
+             System.out.println("not answer");
+         }
+     }
+         public static void main(String[] args) {
+             int n=153;
+             check_armstrong(n);
+             
+         }
+         
+     }
