85bba0c9bff5d97b32353ec87a38b0967838320a
mdsohail12 <mdsohailalam612@gmail.com>
2025-03-12 09:52:58 +0530

    returnsum

---
+++ Array/c.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+// 175=1+7+5
+public class c {
+    public static void print(int n){
+        int sum=0;
+        while(n>0){
+            int rem=n%10;
+         sum=sum+rem;
+            n=n/10;
+        }
+        System.out.println(sum);
+       
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        print(n);
+        sc.close();
+    }
+    
+}
