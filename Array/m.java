a744c4fe2c03bfe1eadd5d955e90c77fd8d48d03
mdsohail12 <mdsohailalam612@gmail.com>
2025-03-17 12:22:30 +0530

    pyrmid

---
+++ Array/m.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class m {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
