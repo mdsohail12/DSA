90db941471f296efe4711dd0ddf612b99da42c15
mdsohail12 <mdsohailalam612@gmail.com>
2025-03-20 14:37:19 +0530

    du

---
+++ Array/D.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+public class D {
+// [1,2,1,2,3,4,4]
+// 0 ,1,2,3,4,5,6
+    public static void findduplicate(int []arr,int size){
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                if(arr[i]==arr[j]){
+                    System.out.println("Dublicate"+arr[i]);
+                    return;
+                }
+            }
+        }
+        System.out.println("No duplicate found");
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter array size");
+        int size=sc.nextInt();
+        int []arr=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        findduplicate(arr,size);
+        // System.out.println(Arrays.toString(arr));
+}
+    
+}
