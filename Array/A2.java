dd2ea4ff999dce05d0e9cf5e3746a6597cec2e4e
mdsohail12 <mdsohailalam612@gmail.com>
2025-03-23 13:47:19 +0530

    Reverse

---
+++ Array/A2.java
@@ -0,0 +1,14 @@
+public class A2 {
+        public static void main(String[] args) {
+            int a=10;
+            int b=20;
+            System.out.println("After Swapping "+a+" "+b);
+    
+            a=a+b;
+            b=a-b;
+            a=a-b;
+            System.out.println("After Swapping "+a+" "+b);
+    
+        
+    }   
+    }
