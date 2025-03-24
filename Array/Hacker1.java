cd1d81c9c0fad395c28560a68dc5784c1bc5ad35
mdsohail12 <mdsohailalam612@gmail.com>
2025-03-24 09:40:01 +0530

    hackerone 1

---
+++ Array/Hacker1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Hacker1 {
+// yha se
+
+
+// public class DataTypes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int testCases = scanner.nextInt();
+        
+        for (int i = 0; i < testCases; i++) {
+            try {
+                long number = scanner.nextLong();
+                System.out.println(number + " can be fitted in:");
+                if (number >= Byte.MIN_VALUE && number <= Byte.MAX_VALUE) {
+                    System.out.println("* byte");
+                }
+                if (number >= Short.MIN_VALUE && number <= Short.MAX_VALUE) {
+                    System.out.println("* short");
+                }
+                if (number >= Integer.MIN_VALUE && number <= Integer.MAX_VALUE) {
+                    System.out.println("* int");
+                }
+                if (number >= Long.MIN_VALUE && number <= Long.MAX_VALUE) {
+                    System.out.println("* long");
+                }
+            } catch (Exception e) {
+                System.out.println(scanner.next() + " can't be fitted anywhere.");
+            }
+        }
+        scanner.close();
+    }
+}
+
+    
+}
