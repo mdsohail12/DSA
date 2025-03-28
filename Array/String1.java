4e58445ae0526cb5ba45859ee24ca3418ef70f1d
mdsohail12 <mdsohailalam612@gmail.com>
2025-03-28 20:18:03 +0530

    String Start Here 2nd

--- Array/String1.java
+++ Array/String1.java
@@ -1,9 +1,16 @@
 public class String1 {
     public static void main(String[] args) {
         String x="Sohail";
-        // System.out.println(x);
+        // Acces the elemet
         System.out.println(x.charAt(0));
-        System.out.println(x.charAt(3));
+         //this is method to use get the length of element
+        System.out.println("the length is "+x.length());
+        // how to iterate the element
+        // for(int i=0;i<x.length();i++){
+        //     System.out.println(x.charAt(i));
+        // }
+        // print the sub string
+        System.out.println(x.substring(2, 5));
             
         }
     }
