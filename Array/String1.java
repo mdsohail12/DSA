db1a3e1976862e3952b65dcd9ab8e404b31c1519
mdsohail12 <mdsohailalam612@gmail.com>
2025-03-28 19:55:01 +0530

    String Start Here

---
+++ Array/String1.java
@@ -0,0 +1,11 @@
+public class String1 {
+    public static void main(String[] args) {
+        String x="Sohail";
+        // System.out.println(x);
+        System.out.println(x.charAt(0));
+        System.out.println(x.charAt(3));
+            
+        }
+    }
+    
+
