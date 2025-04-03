7a185c7f8bf6d355d3ff9039c302439a552c2a28
mdsohail12 <mdsohailalam612@gmail.com>
2025-04-03 23:51:46 +0530

    change

--- Array/Cccc.java
+++ Array/Cccc.java
@@ -1,11 +1,26 @@
+import java.util.Arrays;
+
 public class Cccc {public static void main(String[] args) {
     // int a=10;
     // int b=20;
-    String text="Hello world";
+    // String text="Hello world";
     // String a=text.toUpperCase();
     // System.out.println(a);
-    System.out.println(text.toUpperCase());
-    System.out.println(text.toLowerCase());
+    // System.out.println(text.toUpperCase());
+    // System.out.println(text.toLowerCase());
+    // String name1="Sohail ";
+    // String name2="Alam";
+    // System.out.println(name1.concat(name2));
+    // String txt = "We are the so-called \"Vikings\" from the north.";
+    // System.out.println(txt);
+    // String str="GeeksforGeeks";
+    // System.out.println("Substring is "+str.substring(7));
+    // String str="GeeksforGeeks";
+    // String substring=str.substring(0, 8);
+    // System.out.println("Substring is "+substring);
+    String x="I am a good boy";
+    String []word=x.split(" ");
+    System.out.println(Arrays.toString(word));
 }
     
 }
