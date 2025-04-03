e6ec2254b3078881d13c86fdf34e128c5fcf9a3f
mdsohail12 <mdsohailalam612@gmail.com>
2025-04-03 20:52:17 +0530

    change

--- Array/Cccc.java
+++ Array/Cccc.java
@@ -1,7 +1,11 @@
 public class Cccc {public static void main(String[] args) {
-    int a=10;
-    int b=20;
-    System.out.println(a+b);
+    // int a=10;
+    // int b=20;
+    String text="Hello world";
+    // String a=text.toUpperCase();
+    // System.out.println(a);
+    System.out.println(text.toUpperCase());
+    System.out.println(text.toLowerCase());
 }
     
 }
