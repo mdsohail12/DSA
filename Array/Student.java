a91d52cbe55a893abbec127bac74a6a27fe05dd6
mdsohail12 <mdsohailalam612@gmail.com>
2025-04-04 16:25:46 +0530

    change

---
+++ Array/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    static int a;
+    public void show(){
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+        Student s=new Student();
+        s.show();
+    
+}
+    
+}
