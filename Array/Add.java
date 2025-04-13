67cfffc7b814c20bf9366838c0d46e10fb075389
mdsohail12 <mdsohailalam612@gmail.com>
2025-04-13 06:36:19 +0530

    change h mamah

---
+++ Array/Add.java
@@ -0,0 +1,8 @@
+public class Add {
+    public static void main(String[] args) {
+        int a=0;int b=0;
+        System.out.println(a+b);
+        System.out.println("Hii today is sunday");
+    }
+    
+}
