a4fb909c7ba84baa5c15c9d934f2c8f95b38b608
mdsohail12 <mdsohailalam612@gmail.com>
2025-04-14 22:50:13 +0530

    sohail array list

---
+++ Array/List.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class List {
+    public static void main(String[] args) {
+        ArrayList<String>obj=new ArrayList<>();
+        obj.add("Sohail");
+        obj.add("Soaib");
+        obj.add("junaid Alam");
+        System.out.println(obj.get(0));
+        System.out.println(obj.get(1));
+        System.out.println(obj.get(2));
+
+    }
+    
+}
