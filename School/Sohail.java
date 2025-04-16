c9150a58fb1440ca27aa2556cd3fcb3599097748
mdsohail12 <mdsohailalam612@gmail.com>
2025-04-16 12:15:29 +0530

    Oops Star2

---
+++ School/Sohail.java
@@ -0,0 +1,16 @@
+package School;
+
+public class Sohail {
+    private int roll;
+    private char grade;
+    private double per;
+
+    public void SetData(){
+        roll=21;
+        grade='B';
+        per=2.2;
+}
+public void ShowData(){
+    System.out.println(roll+","+grade+","+per);
+}
+}
---
+++ School/UseSohail.java
@@ -0,0 +1,9 @@
+package School;
+
+public class UseSohail {
+   public static void main(String[] args) {
+    Sohail s=new Sohail();
+    s.SetData();
+    s.ShowData();
+   }
+    }
