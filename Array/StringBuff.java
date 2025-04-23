ebfaf9d682ee36f1d7776fcce78fc59e10d26611
mdsohail12 <mdsohailalam612@gmail.com>
2025-04-23 14:19:28 +0530

    StringBuffer

---
+++ Array/StringBuff.java
@@ -0,0 +1,9 @@
+public class StringBuff {
+public static void main(String[] args) {
+    StringBuffer sb=new StringBuffer("Sohail");
+    System.out.println(sb);
+    System.out.println(sb.length());
+    System.out.println(sb.capacity());
+}
+    
+}
