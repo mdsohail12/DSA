cfbf674571c7915370670827d3c7ba240a3c0c88
mdsohail12 <mdsohailalam612@gmail.com>
2025-04-25 08:18:20 +0530

    change

---
+++ Array/Check1.java
@@ -0,0 +1,8 @@
+public class Check1 {
+    StringBuffer sb=new StringBuffer("Sohail");
+    System.out.println(sb);
+    System.out.println(sb.length());
+    System.out.println(sb.capacity());
+    System.out.println("The capacity is that"+sb.capacity());
+    
+}
