fab86c1bf864f0d97a795c411b4a9e78a46eb40a
mdsohail12 <mdsohailalam612@gmail.com>
2025-04-25 08:16:19 +0530

    change

--- Array/StringBuff.java
+++ Array/StringBuff.java
@@ -4,6 +4,7 @@ public static void main(String[] args) {
     System.out.println(sb);
     System.out.println(sb.length());
     System.out.println(sb.capacity());
+    System.out.println("The capacity is that"+sb.capacity());
 }
     
 }
