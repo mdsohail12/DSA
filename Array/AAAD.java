5027c54e941148d297154140d09aeb821c4fe3aa
mdsohail12 <mdsohailalam612@gmail.com>
2025-04-27 08:42:21 +0530

    change

--- Array/AAAD.java
+++ Array/AAAD.java
@@ -2,6 +2,7 @@ public class AAAD {
     public static void main(String[] args) {
         int a=10;int b=20;
         System.out.println(a+b);
+        System.out.println("kuchh samj nhi aa rha h kya kare  bhai ");
     }
     
 }
