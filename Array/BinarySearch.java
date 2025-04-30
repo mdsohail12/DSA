1f721d434ad093735ae0d9fa746aab7014c96f10
mdsohail12 <mdsohailalam612@gmail.com>
2025-04-30 23:00:39 +0530

    for Accending and Decending both wec an check the order

--- Array/BinarySearch.java
+++ Array/BinarySearch.java
@@ -2,18 +2,32 @@ import java.util.Scanner;
 
 public class BinarySearch {
     public int search(int []arr,int target){
-        int low=0;int high=arr.length;int mid=0;
+        int low=0;int high=arr.length-1;int mid=0;
+        boolean isaccending=arr[low]<arr[high];
+        // System.out.println("check index"+high);
         while(low<=high){
              mid=(low+high)/2;
              if(target==arr[mid]){
                 return mid;
              }
-             if(target<arr[mid])
-             high=mid-1;
-             else
-             low=mid+1;
-        }
-        return -1;
+            //  accending
+             if(isaccending){
+                if (target < arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // decending ke liye
+            else{
+                if (target > arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+             }
+        return -1;//not found
     }
     public static void main(String[] args) {
         BinarySearch obj = new BinarySearch();
@@ -28,8 +42,10 @@ public class BinarySearch {
             arr[i]=sc.nextInt();
         }
        int result=obj.search(arr,target);
-       System.out.println(result);
+       System.out.println("the index of"+result);
         sc.close();
     }
+//  
+
     
 }
