0c30129740dcc97a381e8710d7ddb9d259e00b7a
mdsohail12 <mdsohailalam612@gmail.com>
2025-05-06 12:07:31 +0530

    change7

--- Array/B.java
+++ Array/B.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+
 public class B {
     public static void main(String[] args) {
-        System.out.println("i am a software engineer from bihar");
-        System.out.println("i am a software engineer from bihar");
+        ArrayList<String>cities=new ArrayList<>();
+        cities.add("bhopal");
+        System.out.println(cities);
+
+        cities.add(0,"indore");
+        System.out.println(cities);
+        cities.add(0,"Mp");
+        System.out.println(cities);
+
     }
     
 }
