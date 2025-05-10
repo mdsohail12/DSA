734889a6458556d5010c9b86d0a746dbe1767ec7
mdsohail12 <mdsohailalam612@gmail.com>
2025-05-10 08:36:06 +0530

    msg

--- School/UseStudent2.java
+++ School/UseStudent2.java
@@ -3,6 +3,7 @@ package School;
 public class UseStudent2 {
     public static void main(String[] args) {
         Student2.display("sohail",12,7800);
+        System.out.println("Hii Dear Good morning");
     }
     
 }
