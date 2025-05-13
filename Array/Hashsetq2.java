7499c325ec2c89b29d7d2f56c416761aa2503ff5
mdsohail12 <mdsohailalam612@gmail.com>
2025-05-13 13:33:52 +0530

    avoid duplicate

---
+++ Array/Hashsetq2.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+public class Hashsetq2 {
+    public static void main(String[] args) {
+        // creating the hashset
+        HashSet<String> hs=new HashSet<>();
+        // ad the element
+        hs.add("sohail");
+        hs.add("sohail");
+
+        // displaying the hashset
+        System.out.println("The hasset is"+hs);
+
+    }
+    
+}
