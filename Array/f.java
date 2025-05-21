be90a5c8afaca4b41da4289a5285d5d623bb786b
mdsohail12 <mdsohailalam612@gmail.com>
2025-05-21 21:06:41 +0530

    change

--- Array/f.java
+++ Array/f.java
@@ -2,7 +2,8 @@ public class f {
     public static void main(String[] args) {
         int a=90;
         int b=70;
-        System.out.println(a+b);
+        int z=a-b;
+        System.out.println(z);
     }
     
 }
