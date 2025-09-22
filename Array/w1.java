daf1b1ffb471612fba2119adb6f62b460ce9da9c
mdsohail12 <mdsohailalam612@gmail.com>
2025-09-22 21:51:43 +0530

    1

--- Array/w1.java
+++ Array/w1.java
@@ -1,19 +1,19 @@
 public class w1 {
     public static void main(String[] args) {
-         String originalString = "madam";
-        String reversedString = "";
+    //      String originalString = "madam";
+    //     String reversedString = "";
 
-        // Reverse the string
-        for (int i = originalString.length() - 1; i >= 0; i--) {
-            reversedString = reversedString + originalString.charAt(i);
+    //     // Reverse the string
+    //     for (int i = originalString.length() - 1; i >= 0; i--) {
+    //         reversedString = reversedString + originalString.charAt(i);
+    //     }
+
+    //     // Compare original and reversed strings
+    //     if (originalString.equals(reversedString)) {
+    //         System.out.println(originalString + " is a palindrome.");
+    //     } else {
+    //         System.out.println(originalString +" is not a palindrome.");
+    // }
         }
+}
 
-        // Compare original and reversed strings
-        if (originalString.equals(reversedString)) {
-            System.out.println(originalString + " is a palindrome.");
-        } else {
-            System.out.println(originalString +" is not a palindrome.");
-    }
-    
-}
-}
---
+++ DSA/Question1.java
@@ -0,0 +1,6 @@
+public class Question1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Question cheat sheet");
+    }
+    
+}
