97b500ae49926d09b4a3108baf59428a3ddcc28d
mdsohail12 <mdsohailalam612@gmail.com>
2025-10-04 14:42:06 +0530

    change

--- DSA/Question11.java
+++ DSA/Question11.java
@@ -14,29 +14,82 @@ public class Question11 {
     //               second = next;  
     //         }
 
-  System.out.print("Enter gender (male/female): ");
-        String gender = sc.nextLine();
+//   System.out.print("Enter gender (male/female): ");
+//         String gender = sc.nextLine();
 
-        System.out.print("Enter marital status (married/unmarried): ");
-        String status = sc.nextLine();
+//         System.out.print("Enter marital status (married/unmarried): ");
+//         String status = sc.nextLine();
 
-        System.out.print("Enter age: ");
-        int age = sc.nextInt();
-        sc.nextLine();
+//         System.out.print("Enter age: ");
+//         int age = sc.nextInt();
+//         sc.nextLine();
    
-    if(status.equalsIgnoreCase("married")){
-        System.out.println("given");
-    }
-    else if(gender.equalsIgnoreCase("unmarried")&&age>35){
-        System.out.println("given insurence");
-    }
-    else if(gender.equalsIgnoreCase("unmarried")&&age>30){
-        System.out.println("given");
-    }
-    else System.out.println("not given");
+//     if(status.equalsIgnoreCase("married")){
+//         System.out.println("given");
+//     }
+//     else if(gender.equalsIgnoreCase("unmarried")&&age>35){
+//         System.out.println("given insurence");
+//     }
+//     else if(gender.equalsIgnoreCase("unmarried")&&age>30){
+//         System.out.println("given");
+//     }
+//     else System.out.println("not given");
+
+// int month=sc.nextInt();
+
+// switch (month) {
+//     case 1: case 2: case 11: case 12:System.out.println("winter");
+//     break;
+//     case 3:case 4: case 5: case 6:System.out.println("summer");
+//     break;
+//     case 7:case 8:case 9:case 10:System.out.println("Rainy");
+//     break;
+//     default:System.out.println("invalid");
+//         break;
+// System.out.println("which day you want to see");
+// System.out.println("plz enter number 1 is sun and last day no.4");
+// int month=sc.nextInt();
+
+// switch (month) {
+//     case 1:System.out.println("sun");
+//     break;
+//     case 2:System.out.println("monday");
+//     break;
+//     case 3:System.out.println("tue");
+//     break;
+//     case 4:System.out.println("wed");
+        
+//         break;
+
+//     default:System.out.println("invalid");
+//         break;
+// int a=sc.nextInt();
+// int b=(a>=0)?a:-a;
+// System.out.println(Math.abs(a));
+
+// int input=sc.nextInt();
+// int sum=0;
+// for(int i=0;i<=input;i++){
+//     int digit=input%10;
+//     sum+=digit;
+//     input=input/10;
+// }
+// System.out.println(sum);
+
+System.out.println("plz enter your name");
+String str=sc.nextLine();
+for(int i=str.length()-1;i>=0;i--){
+    char ch=str.charAt(i);
+    System.out.print(ch);
+}
 
 
-             sc.close();
+
+     sc.close();
+}
+
+
+        
 
     }
             
@@ -44,4 +97,4 @@ public class Question11 {
             
             
     
-}
+
