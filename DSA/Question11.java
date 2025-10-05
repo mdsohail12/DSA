df405a5e16df19c9c84e36e189246854c68c20b2
mdsohail12 <mdsohailalam612@gmail.com>
2025-10-05 10:15:29 +0530

    check

--- DSA/Question11.java
+++ DSA/Question11.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Question11 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        // Scanner sc=new Scanner(System.in);
     //     int n=sc.nextInt();
     //         int first=0,second=1;
     //            System.out.println("Fibonacci Series up to " + n + " terms:");
@@ -95,14 +95,21 @@ public class Question11 {
 //     System.out.print(ch);
 // }
 
-for(int i=1;i<=4;i++){
-    for(int j=1;j<=i;j++){
-        System.out.print("*");
+for(int i=68;i>=65;i--){
+    for(int j=i;j>=65;j--){
+        System.out.print((char)j);
     }
     System.out.println();
 }
+// for(int i=1;i<=4;i++){
+//     for(int j=1;j<=i;j++){
+//         System.out.print("*");
+//     }
+//     System.out.println();
+// }
 
-     sc.close();
+    //  sc.close();
+    // System.out.println("hiii");
 }
     }
             
