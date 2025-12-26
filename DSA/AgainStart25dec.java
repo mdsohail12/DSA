a9ed6e8c6ea912d6873066ee808e98bfce0e2dca
mdsohail12 <mdsohailalam612@gmail.com>
2025-12-26 08:03:07 +0530

    add

--- DSA/AgainStart25dec.java
+++ DSA/AgainStart25dec.java
@@ -1,6 +1,10 @@
 public class AgainStart25dec {
     public static void main(String[] args) {
         System.err.println("Agaim start the coding from today and i promised i will do everyday");
+
+        int a=5,b=5;
+        int c=a+b;
+        System.out.println(c);
     }
     
 }
