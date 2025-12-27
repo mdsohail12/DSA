c6edbcad2aa6522202692e6053adb686a271ffce
mdsohail12 <mdsohailalam612@gmail.com>
2025-12-27 10:40:01 +0530

    small

--- DSA/AgainStart25dec.java
+++ DSA/AgainStart25dec.java
@@ -1,13 +1,21 @@
 public class AgainStart25dec {
     public static void main(String[] args) {
+        // int []arr= {1,2,3,4,5};
+        // int largestElement=arr[0];
+        // for(int i=1;i<arr.length;i++){
+        //         if(arr[i]>largestElement){
+        //             largestElement=arr[i];
+        //         }
+        //     }
+        //         System.out.println(largestElement);
         int []arr= {1,2,3,4,5};
-        int largestElement=arr[0];
+        int smallestElement=arr[0];
         for(int i=1;i<arr.length;i++){
-                if(arr[i]>largestElement){
-                    largestElement=arr[i];
+                if(arr[i]<smallestElement){
+                    smallestElement=arr[i];
                 }
             }
-                System.out.println(largestElement);
+                System.out.println(smallestElement);
     }
     }
     
