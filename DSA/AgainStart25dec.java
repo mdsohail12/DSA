f1fc3a49da7127337860b2c5cb7d87d2b0977350
mdsohail12 <mdsohailalam612@gmail.com>
2025-12-27 10:37:07 +0530

    largest

--- DSA/AgainStart25dec.java
+++ DSA/AgainStart25dec.java
@@ -1,10 +1,14 @@
 public class AgainStart25dec {
     public static void main(String[] args) {
-        System.err.println("Agaim start the coding from today and i promised i will do everyday");
-
-        int a=5,b=5;
-        int c=a+b;
-        System.out.println(c);
+        int []arr= {1,2,3,4,5};
+        int largestElement=arr[0];
+        for(int i=1;i<arr.length;i++){
+                if(arr[i]>largestElement){
+                    largestElement=arr[i];
+                }
+            }
+                System.out.println(largestElement);
+    }
     }
     
-}
+
