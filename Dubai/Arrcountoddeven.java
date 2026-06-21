d370192067a211b1e7065faa6720a6c4908f1a8f
mdsohail12 <mdsohailalam612@gmail.com>
2026-06-21 14:09:27 +0530

    count odd evenopyimezed

--- Dubai/Arrcountoddeven.java
+++ Dubai/Arrcountoddeven.java
@@ -10,28 +10,40 @@ public class Arrcountoddeven {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        int evenCount=0;
-        System.out.println("Even no. here");
+        // int evenCount=0;
+        // System.out.println("Even no. here");
 
-        for(int i=0;i<n;i++){
+        // for(int i=0;i<n;i++){
+        //     if(arr[i]%2==0){
+        //         System.out.println(arr[i]+" Even no.");
+        //         evenCount++;
+        //     }
+        // }
+        // System.out.println("Total Even no. is"+evenCount);
+
+        // int oddCount=0;
+        // System.out.println("Odd no. here");
+        // for(int i=0;i<n;i++){
+        //     if(arr[i]%2!=0){
+        //         System.out.println(arr[i]+" odd no.");
+        //        oddCount++;
+        //     }
+        // }
+        // System.out.println("Total odd no. is"+oddCount);
+
+   System.out.println("optimezed privious code ok time complixity o(n) using one loop that why ok");
+        int evenCount=0;
+        int oddCount=0;
+         for(int i=0;i<n;i++){
             if(arr[i]%2==0){
                 System.out.println(arr[i]+" Even no.");
                 evenCount++;
+            }else{
+                oddCount++;
             }
         }
-        System.out.println("Total Even no. is"+evenCount);
-
-        int oddCount=0;
-        System.out.println("Odd no. here");
-        for(int i=0;i<n;i++){
-            if(arr[i]%2!=0){
-                System.out.println(arr[i]+" odd no.");
-               oddCount++;
-            }
-        }
-        System.out.println("Total odd no. is"+oddCount);
+        System.out.println("Total even"+evenCount);
+        System.out.println("total odd"+oddCount);
 
 
-    }
-    
-}
+    }}
