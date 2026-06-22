072f3084f6c2ced13ef15fa5015aae21eb6d4881
mdsohail12 <mdsohailalam612@gmail.com>
2026-06-22 23:59:29 +0530

    third ch

---
+++ Dubai/Arrthird.java
@@ -0,0 +1,34 @@
+package Dubai;
+
+import java.util.Scanner;
+
+public class Arrthird {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int first=Integer.MIN_VALUE;
+        int secondLargest=Integer.MIN_VALUE;
+        int thirdLargest=Integer.MIN_VALUE;
+
+         for(int i=0;i<n;i++){
+        int current=arr[i];
+            if(current>first){
+                thirdLargest=secondLargest;
+                secondLargest=first;
+                first=current;
+            }else if(current > thirdLargest && current != first && current!=secondLargest){
+                thirdLargest=secondLargest;
+                secondLargest=current;
+            }
+        }
+        System.out.println(first+" this is largest ");
+        System.out.println(secondLargest+" this is secondlargest ");
+        System.out.println(thirdLargest+" this is third largest ");
+        sc.close();
+    }
+    
+}
--- Dubai/arrsecondl.java
+++ Dubai/arrsecondl.java
@@ -5,51 +5,29 @@ import java.util.Scanner;
 public class arrsecondl {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        // int n=sc.nextInt();
-        // int []arr=new int[n];
-        // for(int i=0;i<n;i++){
-        //     arr[i]=sc.nextInt();
-        // }
-        // int largest=Integer.MIN_VALUE;
-        // int secondLargest=Integer.MIN_VALUE;
-         
-        // for(int i=0;i<n;i++){
-        // int current=arr[i];
-        //     if(current>largest){
-        //         secondLargest=largest;
-        //         largest=current;
-        //     }else if(current > secondLargest && current != largest){
-        //         secondLargest=current;
-        //     }
-        // }
-        // System.out.println(largest+" this is largest ");
-        // System.out.println(secondLargest+" this is secondlargest ");
-
-
-
-int n=sc.nextInt();
+        int n=sc.nextInt();
         int []arr=new int[n];
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        int first=Integer.MIN_VALUE;
+        int largest=Integer.MIN_VALUE;
         int secondLargest=Integer.MIN_VALUE;
-        int thirdLargest=Integer.MIN_VALUE;
-
-         for(int i=0;i<n;i++){
+         
+        for(int i=0;i<n;i++){
         int current=arr[i];
-            if(current>first){
-                thirdLargest=secondLargest;
-                secondLargest=first;
-                first=current;
-            }else if(current > thirdLargest && current != first && current!=secondLargest){
-                thirdLargest=secondLargest;
+            if(current>largest){
+                secondLargest=largest;
+                largest=current;
+            }else if(current > secondLargest && current != largest){
                 secondLargest=current;
             }
         }
-        System.out.println(first+" this is largest ");
+        System.out.println(largest+" this is largest ");
         System.out.println(secondLargest+" this is secondlargest ");
-        System.out.println(thirdLargest+" this is third largest ");
+
+
+
+
 
 
 
