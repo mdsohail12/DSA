f2de8347ff2e6ee3b43395ce59c48f6839ea82c0
mdsohail12 <mdsohailalam612@gmail.com>
2026-06-28 23:13:41 +0530

    check

---
+++ Dubai/Arrleftrotate.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Arrleftrotate {
+    public static void main(String[] args) {
+        System.out.println("we never left the coding i pledge");
+    }
+    
+}
