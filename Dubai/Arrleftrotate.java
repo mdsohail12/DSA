b842ce47de5d2818ccd12f322c7e9f6681758dbd
mdsohail12 <mdsohailalam612@gmail.com>
2026-06-29 00:52:26 +0530

    left

--- Dubai/Arrleftrotate.java
+++ Dubai/Arrleftrotate.java
@@ -2,7 +2,18 @@ import java.util.Arrays;
 
 public class Arrleftrotate {
     public static void main(String[] args) {
-        System.out.println("we never left the coding i pledge");
+        System.out.println("we never left the coding i am taking a pledge");
+        // we create array
+        int []leftArray={1,2,3,4,5};
+        int first=leftArray[0];
+        // shift left by 1
+        for(int i=0;i<leftArray.length-1;i++){
+            leftArray[i]=leftArray[i+1];
+        }
+        // put first at end
+        leftArray[leftArray.length-1]=first;
+
+        System.out.println(Arrays.toString(leftArray));
     }
     
 }
