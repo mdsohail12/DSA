d5a10ef821f48df2ac3b255aeb452e3408022152
mdsohail12 <mdsohailalam612@gmail.com>
2026-07-04 18:43:10 +0530

    change

--- Dubai/Method.java
+++ Dubai/Method.java
@@ -8,15 +8,25 @@ public class Method {
     //     System.out.println(fname+ " refnes");
     // }
     // with two paramiter
-    public static void myMethod(String fname,int age){
-        System.out.println("My name is "+fname+" and age is  "+ age);
+    // public static void myMethod(String fname,int age){
+    //     System.out.println("My name is "+fname+" and age is  "+ age);
+    // }
+    // start with if else
+    public static void myMethod(int age){
+        if(age<18){
+             System.out.println("Access denied - You are not old enough!");
+        }else{
+            System.out.println("Access granted - You are old enough!");
+        }
+       
     }
     public static void main(String[] args) {
         // myMethod();
         // myMethod("Sohail");
         // myMethod("Soaib");
-        myMethod("Sohail",26);
-        myMethod("Soaib",32);
+        // myMethod("Sohail",26);
+        // myMethod("Soaib",32);
+        myMethod(20);
         
         
     }
