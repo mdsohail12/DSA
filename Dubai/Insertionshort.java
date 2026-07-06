d749fdfd4b0e2fd07d29fefb6ae8a32b38146582
mdsohail12 <mdsohailalam612@gmail.com>
2026-07-07 00:48:44 +0530

    selection

---
+++ Dubai/Insertionshort.java
@@ -0,0 +1,24 @@
+public class Insertionshort {
+    public static void printArray(int []arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(" "+arr[i]);
+        }
+        }
+    public static void main(String[] args) {
+        int []arr={3,7,5,2,1};
+        for(int i=0;i<arr.length-1;i++){
+            int smallest=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[smallest] > arr[j]){
+                    smallest=j;
+                }
+            }
+             int temp=arr[smallest];
+           arr[smallest]=arr[i];
+           arr[i]=temp;
+        }
+
+        printArray(arr);
+    }
+    
+}
