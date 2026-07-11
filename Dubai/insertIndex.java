dff71f12a7dcff838d373f5a1763e94cf7a77e76
mdsohail12 <mdsohailalam612@gmail.com>
2026-07-11 18:13:27 +0530

    Update code

--- Dubai/insertIndex.java
+++ Dubai/insertIndex.java
@@ -3,15 +3,21 @@ import java.util.Arrays;
 public class insertIndex {
     public static void main(String[] args) {
         int [] arr={10,20,30,40,50};
-        int target=35;
+        int target=30;
+        // int target=35;
         int left=0;
         int right=arr.length-1;
-        while (left<right) {
+        while (left<=right) {
             int mid=(left+(right-left)/2);
             // System.out.println(mid);
+            if(arr[mid] == target){
+              System.out.println("If the target is alraedy present then print index "+mid);
+                return;
+            }
 
-            if(arr[mid]<=target){
+            else if(arr[mid]<target){
                 left=mid+1;
+
             }else{
                 right=mid-1;
             }
