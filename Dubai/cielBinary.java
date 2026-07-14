ea0b4e185b98f2afdbeffea2fe376564aa70dd17
mdsohail12 <mdsohailalam612@gmail.com>
2026-07-14 14:01:50 +0530

    ceil final code

--- Dubai/cielBinary.java
+++ Dubai/cielBinary.java
@@ -1,6 +1,6 @@
-public class floorCiel {
+public class cielBinary {
     public static void main(String[] args) {
-        int []arr = {10,20,30,40,50};//Insert Position = 2
+         int []arr = {10,20,30,40,50};//Insert Position = 2
     int target = 25;
     int left=0;
     int right=arr.length-1;
@@ -8,24 +8,20 @@ public class floorCiel {
     int save=-1;
     while (left<=right) {
         int mid=(left+(right-left)/2);
-        System.out.println(mid);
         if(arr[mid]==target){
             save=mid;
             break;
         }
         else if(arr[mid]>target){
-            save=mid;//possible floor
-            right=mid-1;//search larger floor
-        }else{
+            save=mid;
             left=mid+1;
         }
+        else{
+            right=mid-1;
+        }
     }
-   if(save != -1){
-    System.err.println(arr[save]);
-}
-else{
-    System.out.println("Floor not found");
+    
 
-}
-    } 
+    }
+    
 }
