9a8f75bb18a2dcc65bfaa52ba80e498d790074e7
mdsohail12 <mdsohailalam612@gmail.com>
2026-07-17 11:25:12 +0530

    find sorted array even no.

--- Dubai/evenBinarySearch.java
+++ Dubai/evenBinarySearch.java
@@ -1,14 +1,24 @@
 public class evenBinarySearch {
     public static void main(String[] args) {
-        int []arr={1,2,3,4,5,6};
+        int []arr={1,3,5,7,8,10};
         int left=0;
+        int answer=-1;
         int right=arr.length-1;
         System.out.println(right);
         while (left<=right) {
             int mid=(left+(right-left)/2);
             if(arr[mid]%2==0){
-                System.out.println(arr[mid]);
+                answer=mid;//may posiblee first even
+                right=mid-1;
+            }else{
+                left=mid+1;
             }
+        }
+        if(answer!=-1){
+             System.out.println("Index = " + answer);
+            System.out.println("Value = " + arr[answer]);
+        }else{
+             System.out.println("No Even Number Found");
         }
     }
     
