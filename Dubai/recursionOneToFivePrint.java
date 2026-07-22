f37f11fac06d7da3fbe848b503e616aa5032e70a
mdsohail12 <mdsohailalam612@gmail.com>
2026-07-22 11:30:42 +0530

    change

--- Dubai/recursionOneToFivePrint.java
+++ Dubai/recursionOneToFivePrint.java
@@ -1,14 +1,17 @@
 public class recursionOneToFivePrint {
     public static void check(int n){
-        if(n==101){
+        if(n==0){
             return;
         }
         System.out.println(n);
-        check(n+1);
+        n--;
+        check(n);
+        // check(n+1);
+
     }
 
     public static void main(String[] args) {
-        check(1);
+        check(10);
         
     }
     
