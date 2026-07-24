6c2384978d633a8927fb0543965a0ab7b4aa77e3
mdsohail12 <mdsohailalam612@gmail.com>
2026-07-24 12:35:09 +0530

    change factorial

--- Dubai/recursionChange.java
+++ Dubai/recursionChange.java
@@ -1,16 +1,13 @@
-public class recursionOneToFivePrint {
+public class recursionChange {
     public static void check(int n){
-        if(n==11){
+        if(n==6){
             return;
         }
+        check(n+1);
         System.out.println(n);
-       check(n+1);
-
     }
-
     public static void main(String[] args) {
         check(1);
-        
     }
     
 }
---
+++ Dubai/recursionFact.java
@@ -0,0 +1,14 @@
+public class recursionFact {
+    public static int factorial(int n){
+        if(n==0){
+        return 1;
+    }
+    return n * factorial(n - 1);
+    }
+    public static void main(String[] args) {
+       
+       int fact=factorial(5);
+        System.out.println(fact);
+    }
+    
+}
