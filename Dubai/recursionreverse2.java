50a3be9d1a7cdc9964b9ded82aecfdad74cb7719
mdsohail12 <mdsohailalam612@gmail.com>
2026-07-30 12:15:58 +0530

    change

--- Dubai/recursionreverse2.java
+++ Dubai/recursionreverse2.java
@@ -1,12 +1,19 @@
 package Dubai;
 
 public class recursionreverse2 {
+    public static int countDigit(int n){
+        if(n==0){
+        return 0;
+
+        }
+        return 1+countDigit(n/10);
+    }
     public static int printReverse(int n){
         if(n==0){
             return 0;
         }
-        return n%10*1000+printReverse(n/10);
-    }
+         return (int)(n % 10 * Math.pow(10, countDigit(n/10)))+ printReverse(n/10);
+        }
     public static void main(String[] args) {
         int n=123;
        System.out.println( printReverse(n));
