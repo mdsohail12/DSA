d92ad58783f0b9bc2be9e936125a28ffc6718c87
mdsohail12 <mdsohailalam612@gmail.com>
2026-08-05 15:27:48 +0530

    next version

---
+++ Dubai/recursionFibbonacciUpdateVersion.java
@@ -0,0 +1,26 @@
+package Dubai;
+
+public class recursionFibbonacciUpdateVersion {
+    // function 1
+    public static int fib(int n){
+        if(n<=1){
+            return n;
+        }
+        return fib(n-1)+(n-2);
+    }
+    // function 2
+    public static void printseries(int currentIndex,int n){
+        if(currentIndex > n){
+            return;
+        }
+         System.out.println(fib(currentIndex));
+
+        printseries(currentIndex + 1, n);
+    }
+    public static void main(String[] args) {
+        printseries(0,5);
+
+        // System.out.println(fib(5));
+    }
+    
+}
--- Dubai/xx.java
+++ Dubai/xx.java
@@ -3,6 +3,7 @@ package Dubai;
 public class xx {
     public static void main(String[] args) {
         System.out.println("not do code today ");
+        System.out.println("not do code today  and today my mind out off");
     }
     
 }
