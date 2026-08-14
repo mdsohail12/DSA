615382d859a7a89d6067b10a9a112f11bfb1ba66
mdsohail12 <mdsohailalam612@gmail.com>
2026-08-14 12:42:52 +0530

    change

--- Dubai/Student.java
+++ Dubai/Student.java
@@ -1,17 +1,34 @@
 package Dubai;
 
+import java.util.Scanner;
+
 public class Student {
-    private int age;
-   private char grade;
+//     int age;
+// char grade;
+// double per;
+//there is drawback of this code encapsulation rull not follow
+    private int roll;
+    // private char grade;
+    private String name;
     private double per;
 
     public void setData(){
-        age=10;
-        grade='A';
-        per=76.9;
+        //step1
+        // age=10;
+        // grade='A';
+        // per=76.9;
+        //now step2 take input from the user in entity class this is not good ok
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter age,name, per");
+         roll=sc.nextInt();
+         name=sc.next();
+         per=sc.nextDouble();
+       
+
     }
     public void getData(){
-       System.out.println(age + ","+grade +","+per);
+    //    System.out.println(age + ","+grade +","+per);
+       System.out.println("Roll="+roll+",Name="+name+",percentage="+per);
         
     }
     
--- Dubai/UseStudent.java
+++ Dubai/UseStudent.java
@@ -2,9 +2,15 @@ package Dubai;
 
 public class UseStudent {
     public static void main(String[] args) {
-        Student s=new Student();
-        s.setData();
-        s.getData();
+        // Student s=new Student();
+        // s.age=10;
+        // s.grade='A';
+        // s.per=76.9;
+        //   System.out.println(s.age + ","+s.grade +","+s.per);
+    //    In student class all data member by default in Default mood ok and this code fully violation of rull encapsulation and encapsulation says that we cant acces data out off tha class so if we want to acces this data we can use inside metho with getter and setter ok 
+        Student s1=new Student();
+        s1.setData();
+        s1.getData();
 
     }
     
