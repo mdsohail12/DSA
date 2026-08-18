31aa4917fbe7cfcfe38164aa53949532b0a628e1
mdsohail12 <mdsohailalam612@gmail.com>
2026-08-18 13:10:15 +0530

    change

--- Dubai/RevisionAllCode.java
+++ Dubai/RevisionAllCode.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class RevisionAllCode {
 
     public static void main(String[] args) {
@@ -33,30 +35,56 @@ public class RevisionAllCode {
         // }
         // System.out.println(min);
 //Find the missing number in the array
-//         int [] arr={1, 0, 3};
-//        int n=arr.length+1;
-//        int arraySum=0;
-//        int total=n*(n+1)/2;
+    //     int [] arr={1, 0, 3};
+    //    int n=arr.length;
+    //    int arraySum=0;
+    //    int total=n*(n+1)/2;
 
-//        for(int x:arr){
-//         arraySum+=x;
-//        }
-//        int missing=total-arraySum;
-//        System.out.println("mising no is "+ missing);
+    //    for(int x:arr){
+    //     arraySum+=x;
+    //    }
+    //    int missing=total-arraySum;
+    //    System.out.println("mising no is "+ missing);
 
-       int [] arr = {1,2,4,5};
-        int n=arr.length+1;
-        int arraySum=0;
-        int totalSum=n*(n+1)/2;
+    //    int [] arr = {1,2,4,5};
+    //     int n=arr.length+1;
+    //     int arraySum=0;
+    //     int totalSum=n*(n+1)/2;
 
-        for(int x:arr){
-            arraySum+=x;
-        }
-        int missing=totalSum-arraySum;
-        System.out.println(missing+" is the  misiig no. in the array element");
+    //     for(int x:arr){
+    //         arraySum+=x;
+    //     }
+    //     int missing=totalSum-arraySum;
+    //     System.out.println(missing+" is the  misiig no. in the array element");
 
+//find the total  odd and even int the array
+    // int []arr={2, 5, 7, 8};
+    // int even=0;
+    // int odd=0;
+    // for(int i=0;i<=arr.length-1;i++){
+    //     if(arr[i]%2==0){
+    //         even++;
+    //     }
+    //     else{
+    //         odd++;
+    //     }
+    // }
+    // System.out.println("total even is "+even+" total odd is "+odd);
 
-
+    //  reverse the array without using another aarray{1, 2, 3, 4} → {4, 3, 2, 1}
+    int []arr={1, 2, 3, 4};
+  int i=0;
+  int j=arr.length-1;
+  while (i<j) {
+    int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+    i++;
+    j--;
+  }
+            
+        
+        System.out.println(Arrays.toString(arr));
     }
     
 }
