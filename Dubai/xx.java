37f22e92e530b4a72e86f8e8f6ca122f2fb33af9
mdsohail12 <mdsohailalam612@gmail.com>
2026-08-27 22:52:50 +0530

    change

--- Dubai/xx.java
+++ Dubai/xx.java
@@ -1,19 +1,38 @@
 package Dubai;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class xx {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        System.out.println("HEllo now i am again start bro ");
+        // Scanner sc=new Scanner(System.in);
+        // int n=sc.nextInt();
+        // System.out.println("HEllo now i am again start bro ");
         
-        System.out.println("Shal we Start bro ");
-        for(int i=0;i<n;i++){
-            System.out.println("yes");
+        // System.out.println("Shal we Start bro ");
+        // for(int i=0;i<n;i++){
+        //     System.out.println("yes");
 
+        // }
+
+        String str1="madam";
+        String str2="";
+        for(int i=str1.length()-1;i>=0;i--){
+           str2=str2+str1.charAt(i);
+            }
+
+            //this if block wrong answer because here is two address compare not
+            //actual content ok bro thats why getting wrong answer ok bro 
+        if(str1==str2){
+            System.out.println(str1+"  is palindron");
+        }else{
+            System.out.println(str1+" is not palindrome ok");
         }
-        sc.close();
+
+
+         if(str1.equalsIgnoreCase(str2)){
+            System.out.println("Palindrome");
+        }else System.out.println("Not Palindrome");
+        // sc.close();
     }
     
 }
