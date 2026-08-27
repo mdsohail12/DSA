21cb7c5fc19baf79bf601cb9e2d3809aaba921d4
mdsohail12 <mdsohailalam612@gmail.com>
2026-08-27 22:20:01 +0530

    Statr

--- Dubai/xx.java
+++ Dubai/xx.java
@@ -1,9 +1,19 @@
 package Dubai;
 
+import java.util.Scanner;
+
 public class xx {
     public static void main(String[] args) {
-        System.out.println("not do code today ");
-        System.out.println("not do code today  and today my mind out off");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("HEllo now i am again start bro ");
+        
+        System.out.println("Shal we Start bro ");
+        for(int i=0;i<n;i++){
+            System.out.println("yes");
+
+        }
+        sc.close();
     }
     
 }
