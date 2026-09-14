cf99f5aca55ed6875e5abce53da2db71ab092fd1
mdsohail12 <mdsohailalam612@gmail.com>
2026-09-14 18:13:59 +0530

    change

--- Dubai/xx.java
+++ Dubai/xx.java
@@ -489,44 +489,44 @@ public class xx {
 
 
 // moved all zero to end ok 
-// int []arr={0,1,0,3,12};
-// int index=0;
-// int n=arr.length;
-// System.out.println(n);
-// for(int i=0;i<n;i++){
-//     if(arr[i]==0){
-//         continue;
-//     }else{
-//          arr[index]=arr[i];
-//          index++;
-
-//     }
-// }
-//   while (index < arr.length) {
-//             arr[index] = 0;
-//             index++;
-//         }
-// System.out.println(Arrays.toString(arr));
-
-// System.out.println("Now bro all clear code ok with logical");
-
-// moved all zero to begginging ok 
 int []arr={0,1,0,3,12};
 int index=0;
 int n=arr.length;
-System.out.println(n);//5
-for(int i =0;i<n;i++){
-   if (arr[i] == 0) {
+System.out.println(n);
+for(int i=0;i<n;i++){
+    if(arr[i]==0){
+        continue;
+    }else{
+         arr[index]=arr[i];
+         index++;
 
-                int temp = arr[index];
-                arr[index] = arr[i];
-                arr[i] = temp;
-
-                index++;
-            }
-
+    }
 }
+  while (index < arr.length) {
+            arr[index] = 0;
+            index++;
+        }
 System.out.println(Arrays.toString(arr));
+
+System.out.println("Now bro all clear code ok with logical");
+
+// moved all zero to begginging ok 
+// int []arr={0,1,0,3,12};
+// int index=0;
+// int n=arr.length;
+// System.out.println(n);//5
+// for(int i =0;i<n;i++){
+//    if (arr[i] == 0) {
+
+//                 int temp = arr[index];
+//                 arr[index] = arr[i];
+//                 arr[i] = temp;
+
+//                 index++;
+//             }
+
+// }
+// System.out.println(Arrays.toString(arr));
 
 
 
